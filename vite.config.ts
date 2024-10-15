74b0790dcca8f4815bb342f5eaffc59a9579f295
jojy-ul <24070769@studentmail.ul.ie>
2024-10-15 11:27:31 +0100

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/reactgit/',  // Adjust base URL if necessary
+  build: {
+    outDir: 'public',  // Output to public folder
+  },
+})
